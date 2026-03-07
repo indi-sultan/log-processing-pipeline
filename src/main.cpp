bd6666501e16b99e2ccb47bc05f355e5a82219e3
Shivam <shivamsuryathakur123@gmail.com>
2026-03-07 23:15:11 +0530

    Initial commit: logprocessing pipeline project structure

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "setup of project is completed\n";
+    return 0;
+}
---
+++ tests/queue_test.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Queue test running..." << std::endl;
+    return 0;
+}
