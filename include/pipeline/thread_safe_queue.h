6defa022a2866dd52cb8046f7743163f8fab7515
Shivam <shivamsuryathakur123@gmail.com>
2026-03-09 22:21:19 +0530

    added definition of functions in  ThreadSafeQueue class

--- include/pipeline/thread_safe_queue.h
+++ include/pipeline/thread_safe_queue.h
@@ -22,3 +22,42 @@ public:
     bool empty() const;
     size_t size() const;
 };
+
+
+template <typename T>
+void ThreadSafeQueue<T>::push(T value)
+{
+    std::lock_guard<std::mutex> lock(mutex_);
+
+    queue_.push(std::move(value));
+
+    cond_.notify_one();
+}
+
+template <typename T>
+void ThreadSafeQueue<T>::wait_and_pop(T& value)
+{
+    std::unique_lock<std::mutex> lock(mutex_);
+
+    cond_.wait(lock, [this]
+    {
+        return !queue_.empty();
+    });
+
+    value = std::move(queue_.front());
+    queue_.pop();
+}
+
+template <typename T>
+bool ThreadSafeQueue<T>::try_pop(T& value)
+{
+    std::lock_guard<std::mutex> lock(mutex_);
+
+    if(queue_.empty())
+        return false;
+
+    value = std::move(queue_.front());
+    queue_.pop();
+
+    return true;
+}
