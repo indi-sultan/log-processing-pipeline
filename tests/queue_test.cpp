1c3b2ee61d5366998852ac584c72f65442cb0b1c
Shivam <shivamsuryathakur123@gmail.com>
2026-03-11 23:35:05 +0530

    added unit test for ThreadSafeQueue

--- tests/queue_test.cpp
+++ tests/queue_test.cpp
@@ -1,7 +1,18 @@
-#include <iostream>
+#include <gtest/gtest.h>
+#include "pipeline/thread_safe_queue.h"
 
-int main()
+TEST(ThreadSafeQueueTest, PushPop)
 {
-    std::cout << "Queue test running..." << std::endl;
-    return 0;
+    ThreadSafeQueue<int> queue;
+
+    queue.push(1);
+    queue.push(2);
+
+    int value;
+
+    EXPECT_TRUE(queue.try_pop(value));
+    EXPECT_EQ(value, 1);
+
+    EXPECT_TRUE(queue.try_pop(value));
+    EXPECT_EQ(value, 2);
 }
