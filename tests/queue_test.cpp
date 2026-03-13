7d8606c41855d16c35964287d12e3b05326fdd98
Shivam <shivamsuryathakur123@gmail.com>
2026-03-13 23:00:56 +0530

    added ProducerConsumer Test for Queue

--- tests/queue_test.cpp
+++ tests/queue_test.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <thread>
 #include "pipeline/thread_safe_queue.h"
 
 TEST(ThreadSafeQueueTest, PushPop)
@@ -15,4 +16,36 @@ TEST(ThreadSafeQueueTest, PushPop)
 
     EXPECT_TRUE(queue.try_pop(value));
     EXPECT_EQ(value, 2);
+}
+
+TEST(ThreadSafeQueueTest, ProducerConsumer)
+{
+    ThreadSafeQueue<int> queue;
+
+    std::thread producer(
+        [&]()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                queue.push(i);
+            }
+            
+        }
+    );
+    int count = 0;
+    std::thread consumer([&]()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            int value;
+            queue.wait_and_pop(value);
+            count++;
+        }
+        
+    }
+    );
+
+    producer.join();
+    consumer.join();
+    EXPECT_EQ(count, 100);
 }
