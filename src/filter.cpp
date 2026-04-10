19066016143acd0097e9475c92bfba1981c9a0d8
Shivam <shivamsuryathakur123@gmail.com>
2026-04-10 21:29:51 +0530

    added filter class functions definition

---
+++ src/filter.cpp
@@ -0,0 +1,41 @@
+#include "pipeline/filter.h"
+
+void Filter::start()
+{
+    thread_ = std::thread(&Filter::run, this);
+}
+
+void Filter::join()
+{
+    if(thread_.joinable())
+        thread_.join();
+}
+
+Filter::Filter(ThreadSafeQueue<LogEntry>& input_queue,
+               ThreadSafeQueue<LogEntry>& output_queue)
+    : input_queue_(input_queue),
+      output_queue_(output_queue)
+{
+}
+
+void Filter::run()
+{
+    while(true)
+    {
+        LogEntry entry;
+        input_queue_.wait_and_pop(entry);
+
+        //STOP condition
+        if(entry.level == "__STOP__")
+        {
+            output_queue_.push(entry);  // forward STOP
+            break;
+        }
+
+        //Filtering logic
+        if(entry.level == "ERROR")
+        {
+            output_queue_.push(entry);
+        }
+    }
+}
