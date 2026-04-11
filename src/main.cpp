bac96a1b253a4c24268193e389581789c9bcf289
Shivam <shivamsuryathakur123@gmail.com>
2026-04-11 12:32:28 +0530

    output ERROR filtered logs

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,13 @@
 #include "pipeline/thread_safe_queue.h"
 #include "pipeline/reader.h"
 #include "pipeline/parser.h"
+#include "pipeline/filter.h"
 
 int main()
 {
     ThreadSafeQueue<std::string> raw_queue;
     ThreadSafeQueue<LogEntry> parsed_queue;
+    ThreadSafeQueue<LogEntry> error_queue;
 
     int parser_threads = 4;
 
@@ -14,18 +16,35 @@ int main()
 
     Parser parser(raw_queue, parsed_queue, parser_threads);
 
+    Filter filter(parsed_queue, error_queue);
+
     reader.start();
     parser.start();
+    filter.start();
 
     reader.join();
     parser.join();
+    filter.join();
 
-    // Read all parsed logs
-    while(!parsed_queue.empty())
+    // // Read all parsed logs
+    // while(!parsed_queue.empty())
+    // {
+    //     LogEntry entry;
+    //     parsed_queue.try_pop(entry);
+    //     entry.print();
+    // }
+
+    //output filtered logs
+    while (!error_queue.empty())
     {
         LogEntry entry;
-        parsed_queue.try_pop(entry);
-        entry.print();
+        error_queue.try_pop(entry);
+
+        if(entry.level != "__STOP__") //ignore stop signal
+            entry.print();
     }
+    
+
+
     return 0;
 }
